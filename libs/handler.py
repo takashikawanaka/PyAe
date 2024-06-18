bbfaa76f365739d8a21dfedc78df8c85aedf559e
takashikawanaka <takashi.soccer.a@gmail.com>
2024-06-18 21:28:36 +0900

    rename method

--- libs/handler.py
+++ libs/handler.py
@@ -31,10 +31,10 @@ class Handler(PatternMatchingEventHandler):
         )
 
         # ファイルの変更の差異を検出するためのhash置き場
-        self.__cache_hash: dict[str, str] = {}
+        self.__cache_file_hash: dict[str, str] = {}
         for dir, _, files in os.walk(top=self.path):
             for file in files:
-                self.__set_hash(path=os.path.join(dir, file))
+                self.__set_file_hash(path=os.path.join(dir, file))
 
         # プロセス
         self.__process: Callable = process
@@ -42,35 +42,35 @@ class Handler(PatternMatchingEventHandler):
 
     def on_moved(self, event: FileMovedEvent) -> None:
         print(f"File moved, {event.src_path} to {event.src_path}")
-        self.__cache_hash.pop(event.src_path)
-        self.__set_hash(path=event.dest_path)
+        self.__cache_file_hash.pop(event.src_path)
+        self.__set_file_hash(path=event.dest_path)
         self.__process.execute()
 
     def on_created(self, event: FileCreatedEvent) -> None:
         print(f"File created, {event.src_path}")
         # 新規作成
-        self.__set_hash(path=event.src_path)
+        self.__set_file_hash(path=event.src_path)
         self.__process.execute()
 
     def on_deleted(self, event: FileDeletedEvent) -> None:
         print(f"File deleted, {event.src_path}")
         # 削除
-        self.__cache_hash.pop(event.src_path)
+        self.__cache_file_hash.pop(event.src_path)
         self.__process.execute()
 
     def on_modified(self, event: FileModifiedEvent) -> None:
         filepath: str = event.src_path
         hash: str = self.__get_file_hash(path=filepath)
 
-        if hash != self.__cache_hash[filepath]:
+        if hash != self.__cache_file_hash[filepath]:
             # ファイルに変更があった場合、書き換え
             print(f"File modified, {event.src_path}")
-            self.__cache_hash[filepath] = hash
+            self.__cache_file_hash[filepath] = hash
             self.__process.execute()
 
     def __get_file_hash(self, path: str) -> str:
         with open(file=path, mode="rb") as f:
             return md5(string=f.read()).hexdigest()
 
-    def __set_hash(self, path: str) -> str:
-        self.__cache_hash[path] = self.__get_file_hash(path=path)
+    def __set_file_hash(self, path: str) -> str:
+        self.__cache_file_hash[path] = self.__get_file_hash(path=path)
